0ad15abd448e88bfb567bffca76c9b52c3fb0405
Greg von Winckel <greg.von.winckel@gmail.com>
2014-01-15 14:09:38 -0700

    added numpy import and include_dirs to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
-from Cython.Build import cythonize
+import numpy
 import os
 
 
@@ -17,7 +17,8 @@ setup( name = 'lagrange',
        description = 'C++ class for Barycentric Lagrange inerpolation with Cython',
        ext_modules=[Extension("cy_lagrange",
                         sources=["cy_lagrange.pyx","lagrange.cpp"],
-                        language="c++")
+                        language="c++",
+                        include_dirs=[numpy.get_include()])
        ],
        cmdclass = {'build_ext': build_ext},
 )                                         
